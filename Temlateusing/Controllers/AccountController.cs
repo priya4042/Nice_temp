97cdfa00759b50876d3dbb10e59cd0f4a7d5c8cd
ritikrana <ritik.rana@softwebpos.com>
2023-09-12 17:50:28 +0530

    First commit

---
+++ Temlateusing/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Temlateusing.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Loginaccount()
+        {
+            return View();
+        }
+        
+        public ActionResult RegisterAccount()
+        {
+            return View();
+        }
+
+
+    }
+}
